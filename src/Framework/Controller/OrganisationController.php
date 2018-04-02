c1823db718544b87b2734412cda1fe21bd0b7cb3
John Saunders <john@yettimedia.co.uk>
2018-04-02 22:59:41 +0100

    swapped to using integrations

--- src/Framework/Controller/OrganisationController.php
+++ src/Framework/Controller/OrganisationController.php
@@ -5,132 +5,133 @@ namespace DevPledge\Framework\Controller;
 
 use DevPledge\Application\Command\CreateOrganisationCommand;
 use DevPledge\Domain\User;
+use DevPledge\Integrations\Command\Dispatch;
 use DevPledge\Uuid\Uuid;
 use League\Tactician\CommandBus;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use DevPledge\Application\Repository\Organisation\OrganisationRepository;
 
-class OrganisationController
-{
+class OrganisationController {
 
-    /**
-     * @var OrganisationRepository
-     */
-    private $organisationRepository;
+	/**
+	 * @var OrganisationRepository
+	 */
+	private $organisationRepository;
 
-    /**
-     * OrganisationController constructor.
-     * @param OrganisationRepository $organisationRepository
-     */
-    public function __construct(OrganisationRepository $organisationRepository)
-    {
-        $this->organisationRepository = $organisationRepository;
-    }
+	/**
+	 * OrganisationController constructor.
+	 *
+	 * @param OrganisationRepository $organisationRepository
+	 */
+	public function __construct( OrganisationRepository $organisationRepository ) {
+		$this->organisationRepository = $organisationRepository;
+	}
 
-    /**
-     * @param Request $req
-     * @param Response $res
-     * @param CommandBus $bus
-     * @return Response
-     */
-    public function postOrganisation(Request $req, Response $res, CommandBus $bus)
-    {
-        $body = $req->getParsedBody();
-        $userId = $body['user_id'] ?? null;
-        if ($userId === null) {
-            return $res->withJson([
-                'Missing user_id'
-            ], 400);
-        }
+	/**
+	 * @param Request $req
+	 * @param Response $res
+	 * @param CommandBus $bus
+	 *
+	 * @return Response
+	 */
+	public function postOrganisation( Request $req, Response $res, CommandBus $bus ) {
+		$body   = $req->getParsedBody();
+		$userId = $body['user_id'] ?? null;
+		if ( $userId === null ) {
+			return $res->withJson( [
+				'Missing user_id'
+			], 400 );
+		}
 
-        $name = $body['name'] ?? null;
-        if ($name === null) {
-            return $res->withJson([
-                'Missing name'
-            ], 400);
-        }
+		$name = $body['name'] ?? null;
+		if ( $name === null ) {
+			return $res->withJson( [
+				'Missing name'
+			], 400 );
+		}
 
-        $userUuid = new Uuid($userId);
-        $user = new User(); // TODO: Get user from $userUuid
+		$userUuid = new Uuid( $userId );
+		$user     = new User(); // TODO: Get user from $userUuid
 
-        // See CommandHandler\CreateOrganisationHandler
-        $command = new CreateOrganisationCommand($user, $name);
-        $organisation = $bus->handle($command);
-        return $res->withJson($organisation);
-    }
+		// See CommandHandler\CreateOrganisationHandler
+		$command      = new CreateOrganisationCommand( $user, $name );
+		$organisation = Dispatch::command( $command );
 
-    /**
-     * @param Request $req
-     * @param Response $res
-     * @return Response
-     */
-    public function getOrganisation(Request $req, Response $res)
-    {
-        $organisationId = $req->getParam('id');
-        if ($organisationId === null) {
-            return $res->withJson([
-                'Missing ID'
-            ], 400);
-        }
+		return $res->withJson( $organisation );
+	}
 
-        $organisation = $this->organisationRepository->getOrganisation($organisationId);
-        if ($organisation === null) {
-            return $res->withJson([
-                'Organisation not found'
-            ], 404);
-        }
+	/**
+	 * @param Request $req
+	 * @param Response $res
+	 *
+	 * @return Response
+	 */
+	public function getOrganisation( Request $req, Response $res ) {
+		$organisationId = $req->getParam( 'id' );
+		if ( $organisationId === null ) {
+			return $res->withJson( [
+				'Missing ID'
+			], 400 );
+		}
 
-        return $res->withJson($organisation);
-    }
+		$organisation = $this->organisationRepository->getOrganisation( $organisationId );
+		if ( $organisation === null ) {
+			return $res->withJson( [
+				'Organisation not found'
+			], 404 );
+		}
 
-    /**
-     * @param Request $req
-     * @param Response $res
-     * @return Response
-     */
-    public function getOrganisations(Request $req, Response $res)
-    {
-        $filters = $req->getParams([
-            'id',
-            'firstName',
-            'lastName',
-            'email',
-        ]);
+		return $res->withJson( $organisation );
+	}
 
-        $organisations = $this->organisationRepository->searchOrganisations($filters);
+	/**
+	 * @param Request $req
+	 * @param Response $res
+	 *
+	 * @return Response
+	 */
+	public function getOrganisations( Request $req, Response $res ) {
+		$filters = $req->getParams( [
+			'id',
+			'firstName',
+			'lastName',
+			'email',
+		] );
 
-        return $res->withJson($organisations);
-    }
+		$organisations = $this->organisationRepository->searchOrganisations( $filters );
 
-    /**
-     * @param Request $req
-     * @param Response $res
-     * @return Response
-     */
-    public function patchOrganisation(Request $req, Response $res)
-    {
-        $organisationId = $req->getParam('id');
-        if ($organisationId === null) {
-            return $res->withJson([
-                'Missing ID'
-            ], 400);
-        }
+		return $res->withJson( $organisations );
+	}
 
-        $organisation = $this->organisationRepository->getOrganisation($organisationId);
-        if ($organisation === null) {
-            return $res->withJson([
-                'OrganisationController not found'
-            ], 404);
-        }
+	/**
+	 * @param Request $req
+	 * @param Response $res
+	 *
+	 * @return Response
+	 */
+	public function patchOrganisation( Request $req, Response $res ) {
+		$organisationId = $req->getParam( 'id' );
+		if ( $organisationId === null ) {
+			return $res->withJson( [
+				'Missing ID'
+			], 400 );
+		}
 
-        $body = $req->getParsedBody();
+		$organisation = $this->organisationRepository->getOrganisation( $organisationId );
+		if ( $organisation === null ) {
+			return $res->withJson( [
+				'OrganisationController not found'
+			], 404 );
+		}
 
-        // todo : set organisation values from body
+		$body = $req->getParsedBody();
 
-        $organisation = $this->organisationRepository->saveOrganisation($organisation);
+		// todo : set organisation values from body
 
-        return $res->withJson($organisation);
-    }
+		$organisation = $this->organisationRepository->saveOrganisation( $organisation );
+
+		return $res->withJson( $organisation );
+	}
 
 }
