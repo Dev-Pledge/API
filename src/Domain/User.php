80abaf141b1b7064b5f59a90a7cba9f80c13fd10
Jim Wright <jim@linx.net>
2018-04-03 18:51:43 +0100

    Add User Model

--- src/Domain/User.php
+++ src/Domain/User.php
@@ -1,15 +1,85 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Tom
- * Date: 13/02/2018
- * Time: 22:09
- */
 
 namespace DevPledge\Domain;
 
 
+use DevPledge\Uuid\Uuid;
+
 class User
 {
+    /**
+     * @var Uuid
+     */
+    private $id;
 
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime|null
+     */
+    private $updatedAt;
+
+    /**
+     * User constructor.
+     * @param Uuid $id
+     * @param \DateTime $createdAt
+     * @param \DateTime $updatedAt
+     */
+    public function __construct(Uuid $id, \DateTime $createdAt, ?\DateTime $updatedAt)
+    {
+        $this->id = $id;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @return Uuid
+     */
+    public function getId(): Uuid
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param Uuid $id
+     */
+    public function setId(Uuid $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt(\DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime|null $updatedAt
+     */
+    public function setUpdatedAt(?\DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
 }
