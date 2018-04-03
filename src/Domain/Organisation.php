629eb0b157c9a44e4d067d64656b6f182a8301bd
Jim Wright <jim@linx.net>
2018-04-03 18:53:06 +0100

    Add Organisation Model

---
+++ src/Domain/Organisation.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace DevPledge\Domain;
+
+
+use DevPledge\Uuid\Uuid;
+
+class Organisation
+{
+    /**
+     * @var Uuid
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime|null
+     */
+    private $updatedAt;
+
+    /**
+     * User constructor.
+     * @param Uuid $id
+     * @param string $name
+     * @param \DateTime $createdAt
+     * @param \DateTime $updatedAt
+     */
+    public function __construct(Uuid $id, string $name, \DateTime $createdAt, ?\DateTime $updatedAt)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @return Uuid
+     */
+    public function getId(): Uuid
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param Uuid $id
+     */
+    public function setId(Uuid $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt(\DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime|null $updatedAt
+     */
+    public function setUpdatedAt(?\DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}
