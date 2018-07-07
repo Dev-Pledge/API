75882ead58ad58e9571c0af32f118c90d631c489
John Saunders <john@yettimedia.co.uk>
2018-07-07 13:42:07 +0100

    delete

--- dotenv.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dotEnvFile = '.developmentenv';
-if ( isset( $_ENV['production'] ) && $_ENV['production'] == "true" ) {
-	$dotEnvFile = '.productionenv';
-}
-
-//https://github.com/vlucas/phpdotenv
-$dotEnv = new \Dotenv\Dotenv( __DIR__ . '/', $dotEnvFile );
-$dotEnv->load();
