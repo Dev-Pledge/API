69a266654938bc8c230978c7f492d3a314e4e8d7
John Saunders <john@yettimedia.co.uk>
2018-07-19 00:23:58 +0100

    fix maps

--- sql/topic_problem_maps.sql
+++ sql/topic_problem_maps.sql
@@ -4,6 +4,19 @@ CREATE TABLE topic_problem_maps
 (
   topic      VARCHAR(100) NOT NULL,
   problem_id VARCHAR(50)  NOT NULL,
+  created    DATETIME     NOT NULL,
   PRIMARY KEY (topic, problem_id)
 )
   ENGINE = InnoDB;
+
+CREATE INDEX topic_index
+  ON topic_problem_maps (topic);
+
+CREATE INDEX topic_created_index
+  ON topic_problem_maps (topic,created);
+
+CREATE INDEX problem_id_created_index
+  ON topic_problem_maps (problem_id,created);
+
+CREATE INDEX problem_id_index
+  ON topic_problem_maps (problem_id);
