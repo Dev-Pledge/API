09b2e61b448c9ddf36358d35853103aec09449d1
John Saunders <john@yettimedia.co.uk>
2018-08-15 11:02:37 +0100

    stripe settings

---
+++ src/Framework/Settings/StripeSettings.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace DevPledge\Framework\Settings;
+
+use DevPledge\Integrations\Setting\AbstractSetting;
+use Slim\Container;
+
+/**
+ * Class StripeSettings
+ * @package DevPledge\Framework\Settings
+ */
+class StripeSettings extends AbstractSetting {
+	/**
+	 * @var string
+	 */
+	protected $publicApiKey;
+	/**
+	 * @var string
+	 */
+	protected $privateApiKey;
+
+
+	/**
+	 * FixerIO constructor.
+	 */
+	public function __construct() {
+		parent::__construct( 'Stripe' );
+	}
+
+
+	/**
+	 * @param Container $container
+	 *
+	 * @return FixerIOSettings
+	 */
+	public function __invoke( Container $container ) {
+		$this->publicApiKey  = getenv( 'STRIPE_PUBLIC' );
+		$this->privateApiKey = getenv( 'STRIPE_PRIVATE' );
+
+		return $this;
+	}
+
+	/**
+	 * @return FixerIOSettings
+	 * @throws \Interop\Container\Exception\ContainerException
+	 */
+	static public function getSetting() {
+		return static::getFromContainer();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPrivateApiKey(): string {
+		return $this->privateApiKey;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPublicApiKey(): string {
+		return $this->publicApiKey;
+	}
+
+
+}
