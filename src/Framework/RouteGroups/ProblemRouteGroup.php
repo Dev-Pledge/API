cbf341e18ea03cb88d24cf13e90ae03c8c31f94f
John Saunders <john@yettimedia.co.uk>
2018-08-23 23:48:51 +0100

    clean up

--- src/Framework/RouteGroups/ProblemRouteGroup.php
+++ src/Framework/RouteGroups/ProblemRouteGroup.php
@@ -6,7 +6,6 @@ namespace DevPledge\Framework\RouteGroups;
 use DevPledge\Framework\Controller\Pledge\PledgeController;
 use DevPledge\Framework\Controller\Problem\ProblemController;
 use DevPledge\Framework\Controller\Problem\SolutionController;
-use DevPledge\Framework\Middleware\OriginPermission;
 use DevPledge\Framework\Middleware\ResourcePermission;
 use DevPledge\Integrations\Middleware\JWT\Authorise;
 use DevPledge\Integrations\Route\AbstractRouteGroup;
