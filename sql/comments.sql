0cf5b6fb36a0efab6faccf4e79ecf7789aef3265
John Saunders <john@yettimedia.co.uk>
2018-09-01 11:25:00 +0100

    migrations waiting for mysql

---
+++ sql/comments.sql
@@ -0,0 +1,17 @@
+USE api;
+
+CREATE TABLE comments
+(
+  comment_id VARCHAR(50) NOT NULL
+    PRIMARY KEY,
+  comment    TINYTEXT    NOT NULL,
+  entity_id  VARCHAR(50) NOT NULL,
+  user_id    VARCHAR(50) NOT NULL,
+  modified   DATETIME    NOT NULL,
+  created    DATETIME    NOT NULL
+)
+  ENGINE = InnoDB
+  CHARSET = utf8;
+
+CREATE INDEX comment_created_entity
+  ON comments (entity_id, created);
--- src/CliCommands/StartMigrations.php
+++ src/CliCommands/StartMigrations.php
@@ -1,8 +1,18 @@
 <?php
-
+require __DIR__ . '/../../vendor/autoload.php';
 echo 'starting migrations' . PHP_EOL;
-sleep( 2 );
-echo 'cd ' . __DIR__ . '/../../ && vendor/bin/phinx migrate -e development'.PHP_EOL;
-sleep( 2 );
+$isThere = null;
+while ( $isThere === null ) {
+	try {
+		$dsn     = 'mysql:dbname=' . getenv( 'PHINX_DBNAME' ) . ';host=' . getenv( 'PHINX_DBHOST' );
+		$db      = new \TomWright\Database\ExtendedPDO\ExtendedPDO( $dsn, getenv( 'PHINX_DBUSER' ), getenv( 'PHINX_DBPASS' ) );
+		$isThere = $db->dbQuery( "SHOW TABLES;" );
+	} catch ( PDOException $exception ) {
+		echo $exception->getMessage() . PHP_EOL;
+	}
+	sleep( 1 );
+}
+//echo 'cd ' . __DIR__ . '/../../ && vendor/bin/phinx migrate -e development' . PHP_EOL;
+
 echo 'go migrations go - - - ->' . PHP_EOL;
 echo shell_exec( 'cd ' . __DIR__ . '/../../ && vendor/bin/phinx migrate -e development' ) . PHP_EOL;
